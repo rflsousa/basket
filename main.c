53adba796af2ced712020a894d61913dbf656d39
Rafael Sousa <rafael.oliveira.osv@gmail.com>
2019-05-26 16:03:22 -0300

    Criação da função maiorMenorAltura

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float altura[10], media;
+float altura[10], media, maiorAltura = 0, menorAltura = 3;
 char nome[10][30];//numero de linhas e caracteres
 int c; //contador
 
@@ -31,11 +31,30 @@ float mediaAlturas()
     return media;
 }
 
+float maiorMenorAltura()
+{
+    for(c = 0; c < 10; c++)
+    {
+        if(altura[c] > maiorAltura)
+        {
+            maiorAltura = altura[c];
+        }
+
+        else if(altura[c] < menorAltura)
+        {
+            menorAltura = altura[c];
+        }
+    }
+
+    printf("\nMaior altura: %.2f\n", maiorAltura);
+    printf("\nMenor altura: %.2f\n", menorAltura);
+
+    return maiorAltura, menorAltura;
+
+}
+
 int main()
 {
-
-
-
     printf(" APS DE TECNICAS DE PROGRAMACAO\n\n");
     printf(" NOME DO ALUNO: Jhonas Fernandes Souto\tRA: 21106806");
 
@@ -64,6 +83,7 @@ int main()
 
     dadosJogadores();
     mediaAlturas();
+    maiorMenorAltura();
 
     return 0;
 
